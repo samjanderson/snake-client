2cc2b5a60b9969ffa362856defcd8b557f6f3aba
samjanderson <sam.andersonnn@live.ca>
2021-01-13 21:21:58 +0000

    made separate play and client files

---
+++ client.js
@@ -0,0 +1,25 @@
+//  const x = require('./connect');
+//const connect = require(../snake-client)
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '135.23.222.131',
+    port: 50542
+  });
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  return conn;
+}
+
+module.exports = {
+  connect
+};
+
+
+
--- play.js
+++ play.js
@@ -1,22 +1,7 @@
 const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50542
-  });
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  return conn;
-}
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+//  module.exports = connect
