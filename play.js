444c8723d8a0788ff3802fa637b147ddf727d183
samjanderson <sam.andersonnn@live.ca>
2021-01-13 23:14:41 +0000

    setup key to exit game with control c

--- play.js
+++ play.js
@@ -4,6 +4,33 @@ const { connect } = require('./client');  //connect looked like this before { co
 console.log('Connecting ...');
 connect(); //calling connect function we imported from client js
 
+// * Setup User Interface 
+// * Specifically, so that we can handle user input via stdin
+// */
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = function (key) {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  }
+
+  // stdin.on('data', (key) => {
+  //   if (key === '\u0003') {
+  //     process.exit();
+  //   }
+  // });
+
+  stdin.on('data', (key) => handleUserInput(key));
+
+  return stdin;
+}
+
+setupInput();
 //  module.exports = connect
 //play is like a table of operations calling on different functions importing stuff from different modules etc
 //the purpose of not running connect() in client.js is to have more modular code
