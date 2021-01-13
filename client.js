81b2a5456cb8f07207fa861a69f953090cc0af8d
samjanderson <sam.andersonnn@live.ca>
2021-01-13 22:58:45 +0000

    played around with movements

--- client.js
+++ client.js
@@ -1,35 +1,36 @@
 //  const x = require('./connect');
 //const connect = require(../snake-client)
-const net = require('net');
-
+const net = require('net'); //require is importing a different file into your current file ./ importing local file ../ parent folder
+//if no dots or slashes importing an actual module, net module does networking to allow connection between client and server
 const connect = function() {
-  const conn = net.createConnection({ 
-    host: '135.23.222.131',
-    port: 50542
-  });
+  const conn = net.createConnection({ //creates a connection obj you can use to do other functions
+    host: '135.23.222.131', //host and port means this is the server were connection 
+    port: 50542          //only need to do this one time when importing net module
+  });                   //6-9 are instructions for how we can find the server and talk to it
 
   conn.on('connect', () => {
-    console.log('Successfully connected to the name server')
+    console.log('Successfully connected to the name server') //11-13 happens locally on your machine
   })
 
-
-conn.on('connect', () => {
-  conn.write('Name: SJA');
+//.on(condition, callback)
+conn.on('connect', () => {      //on is an event handler so whenever a specific event happens it runs a callback function
+  conn.write('Move: up');
+  conn.write('Name: SJA');     //conn.write is what we are sending to the server
 });
 
-  conn.on('data', (data) => {
+  conn.on('data', (data) => {     //when we receive data just console log this data
     console.log('Server says: ', data);
   });
 
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
 
-  return conn;
+  return conn; //this entire connect functions is to return this connection object
 }
 
-module.exports = {
+module.exports = { 
   connect
 };
-
-
+//the connect in here refers to the entire connect function module.export is an objec
+//we are redefining the module.exports object and creating a connect key which holds the connect value inside of it so we can shorthand
 
--- play.js
+++ play.js
@@ -1,7 +1,9 @@
-const net = require('net');
+// const net = require('net');
 
-const { connect } = require('./client');
+const { connect } = require('./client');  //connect looked like this before { connect } //this will return whole connect function
 console.log('Connecting ...');
-connect();
+connect(); //calling connect function we imported from client js
 
 //  module.exports = connect
+//play is like a table of operations calling on different functions importing stuff from different modules etc
+//the purpose of not running connect() in client.js is to have more modular code
